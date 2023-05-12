c560d2c7c42dbb1f50bb89eadc81281ceaee1123
offsana <offsana.p@gmail.com>
2023-05-12 11:30:45 +0200

    изменения в дз

--- src/main/java/lesson1/Main1.java
+++ src/main/java/lesson1/Main1.java
@@ -14,13 +14,9 @@ public class Main1 {
         System.out.println("Введите второе слово");
         String word2 = main1.nextLine();
 
-        //делим два слова пополам и соединяем
-        final int mid = word1.length() / 2;
-        final int mid2 = word2.length() / 2; //get the middle of the String
-        String[] parts = {word1.substring(0, mid),word2.substring(mid2)};
-        //System.out.println(parts[0]); //first part
-        //System.out.println(parts[1]);
-        System.out.println ((parts[0])+(parts[1]));
+        System.out.println(word1.substring(0,word1.length()/2) + word2.substring( word1.length()/2));
+
+
 
 
 
