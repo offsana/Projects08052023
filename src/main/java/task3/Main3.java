2c9865e981835382375a8aba7817b5ecd27c798b
offsana <offsana.p@gmail.com>
2023-05-12 11:50:25 +0200

    изменения в дз

--- src/main/java/task3/Main3.java
+++ src/main/java/task3/Main3.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Main3 {
     public static void main(String[] args) {
+
+
         Scanner сonwert = new Scanner(System.in);
         System.out.println("Введите сумму в долларах");
         String euro = сonwert.nextLine();
-        double result = (double) sum1(Integer.parseInt(euro),1.09);
-        System.out.print("Сумма в евро "+result);
+
+        double result = sum1(Integer.parseInt(euro),1.09);
+        System.out.print("Сумма в евро "+ result);
 
     }
     public static double sum1 (double a,double b){
-    double c = (a*b);
+    double c = a * b;
     return c;
     }
 }
